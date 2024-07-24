902e2b4f007e4969a2cf7040fcc76f78419a8c20
emmahdez <emma.hernandez.2023@uni.strath.ac.uk>
2024-07-24 15:00:27 +0100

    added markers to map

--- app/tabs/map.jsx
+++ app/tabs/map.jsx
@@ -1,8 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native'
 import MapView, { Marker } from 'react-native-maps';
-import React from 'react'
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
+
 
 const Map = () => {
+  const [locations,setLocations] = useState([]);
+
+  useEffect( () => {
+    const fetchLocations = async () => {
+      try {
+        const response = await axios.get('http://192.168.1.91:8000/locations/');
+        setLocations(response.data);
+      } catch(error) {
+        console.error(error);
+      }
+    };
+    fetchLocations();
+  }, []);
 
 return (
   <View style={styles.container}>
@@ -11,6 +26,16 @@ return (
       showsUserLocation={true}
       provider={MapView.PROVIDER_DEFAULT}
     >
+      {locations.map(location => (
+          <Marker
+            key={location.beach_name}
+            coordinate={{
+              latitude: parseFloat(location.latitude),
+              longitude: parseFloat(location.longitude),
+            }}
+            title={location.beach_name}
+          />
+        ))}
     </MapView>
   </View>
 );
